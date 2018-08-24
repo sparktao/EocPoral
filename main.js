952374d52784902c734bf7a9921b722cca05cd1b
Tao Jinlin <jinlin.tao@hexagonsi.com>
2018-08-24 08:39:55 +0800

    地图功能更新，包括多种标注和救援力量地理位置实时刷新

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ define([
   "luciad/view/style/PinEndPosition",
   "luciad/reference/ReferenceProvider",
   "luciad/shape/ShapeFactory",
+  "luciad/model/store/MemoryStore",
   "luciad/view/feature/FeatureLayer",
   "luciad/view/feature/FeaturePainter",
   "../common/URLUtil",
@@ -23,6 +24,7 @@ define([
             PinEndPosition,
             ReferenceProvider,
             ShapeFactory,
+            MemoryStore,
             FeatureLayer,
             FeaturePainter,
             URLUtil,
@@ -110,37 +112,28 @@ define([
 	  label: "San Francisco Buildings"
 	});
 	}
-  
+
+	//CustomLayerLoad Begin//
+	var iconFeatureLayerDefaultHeight = 300;
+	var iconFeatureLayerDefaultIconSize = 32;
 	function createEventPainter(options) {
 		options = options || {};
-		//#snippet memoization
 		var eventPainter = new FeaturePainter();
-		eventPainter.cache = {};
 		eventPainter.paintBody = function(geoCanvas, feature, shape, layer, map, state) {
-			var dimension = this.getIconSize(feature);
-			var icon = this.cache[dimension];
-			if (!icon || state.selected) {
-			  icon = IconFactory.circle({
-				stroke: state.selected ? "rgba(0,0,255,1.0)" : "rgba(255,255,255,1.0)",
-				fill: state.selected ? "rgba(255,0,255,0.7)" : "rgba(255,0,0,0.5)",
-				strokeWidth: state.selected ? 3 : 2,
-				width: dimension,
-				height: dimension
-			  });
-			  if (!state.selected) {
-				this.cache[dimension] = icon;
-			  }
-			}
-			//#endsnippet memoization
-			shape.focusPoint.z = 300;
+			shape.focusPoint.z = iconFeatureLayerDefaultHeight;
 			geoCanvas.drawIcon(shape.focusPoint, {
-			  width: dimension + "px",
-			  height: dimension + "px",
-			  image: icon
+			  width: iconFeatureLayerDefaultIconSize + "px",
+			  height: iconFeatureLayerDefaultIconSize + "px",
+			  url: state.selected?"themePic/eventSelect.png" : "themePic/event_" + feature.properties.TYPE + ".png"
+			  //anchorX : 0 + "px"
 			});
-			//#snippet memoization
+			geoCanvas.drawShape(ShapeFactory.createPolyline(shape.reference, [shape.focusPoint, ShapeFactory.createPoint(shape.reference, [shape.focusPoint.x, shape.focusPoint.y])]), {
+				stroke: {
+				color: "rgb(128,128,128)",
+				width: 1
+				}
+			});
 		};
-		//#endsnippet memoization
 
 		var emptyStyle = {
 			offset: 20,
@@ -152,53 +145,186 @@ define([
 		var htmlTemplateNormal = '<div style="background-color: rgb(40,40,40); padding: 5px; border-radius: 10px; color: rgb(238,233,233)">$name, $type</div>';
 		var htmlTemplateSelect = '<div style="background-color: rgb(80,10,10); padding: 5px; border-radius: 10px; color: rgb(238,233,233)">$name, $type</div>';
 		eventPainter.paintLabel = function(labelCanvas, feature, shape, layer, map, state) {
-		var html;
-		if (state.selected || options.labelsAlwaysVisible) {
-		  html = state.selected ? htmlTemplateSelect : htmlTemplateNormal;
-		  html = html.replace('$name', feature.properties.NAME);
-		  html = html.replace('$type', feature.properties.TYPE);
-		  labelCanvas.drawLabel(html, shape, emptyStyle);
-		  
-		  if(state.selected)
-			alert(feature.properties.NAME);
-		}
-	};
-
-	eventPainter.getIconSize = function(feature) {
-		//return Math.round(Math.sqrt(Math.round(feature.properties.TOT_POP / 10000) * 10000 / Math.PI) / 28);
-		return 20;
-	};
-	return eventPainter;
+			var html;
+			if (state.selected || options.labelsAlwaysVisible) {
+			  html = state.selected ? htmlTemplateSelect : htmlTemplateNormal;
+			  html = html.replace('$name', feature.properties.NAME);
+			  html = html.replace('$type', feature.properties.TYPE);
+			  labelCanvas.drawLabel(html, shape, emptyStyle);
+			  
+			  if(state.selected)
+			  {
+				alert("eventSelected");
+				
+				removeTempCircle();
+				createTempCircle(ShapeFactory.createPoint(shape.reference, [shape.focusPoint.x, shape.focusPoint.y]), 500);
+			  }
+			}
+		};
+		return eventPainter;
 	};
 	
-	function createEventLayer() {
-	  /************************** Event Layer *****************************/
-	  var eventStore = new UrlStore({
-		target: "data/events.json"
-	  });
+	function createAlarmPainter(options) {
+		options = options || {};
+		var eventPainter = new FeaturePainter();
+		eventPainter.paintBody = function(geoCanvas, feature, shape, layer, map, state) {
+			shape.focusPoint.z = iconFeatureLayerDefaultHeight;
+			geoCanvas.drawIcon(shape.focusPoint, {
+			  width: iconFeatureLayerDefaultIconSize + "px",
+			  height: iconFeatureLayerDefaultIconSize + "px",
+			  url: state.selected?"themePic/alarmSelect.png" : "themePic/alarm.png"
+			  //anchorX : 0 + "px"
+			});
+			geoCanvas.drawShape(ShapeFactory.createPolyline(shape.reference, [shape.focusPoint, ShapeFactory.createPoint(shape.reference, [shape.focusPoint.x, shape.focusPoint.y])]), {
+				stroke: {
+				color: "rgb(128,128,128)",
+				width: 1
+				}
+			});
+		};
 
-	  var eventModel = new FeatureModel(eventStore, {
-		reference: CRS84
-	  });
+		var emptyStyle = {
+			offset: 20,
+			pin: {
+			  endPosition: PinEndPosition.MIDDLE,
+			  width: 1
+			}
+		};
+		var htmlTemplateNormal = '<div style="background-color: rgb(40,40,40); padding: 5px; border-radius: 10px; color: rgb(238,233,233)">$name</div>';
+		var htmlTemplateSelect = '<div style="background-color: rgb(80,10,10); padding: 5px; border-radius: 10px; color: rgb(238,233,233)">$name</div>';
+		eventPainter.paintLabel = function(labelCanvas, feature, shape, layer, map, state) {
+			var html;
+			if (state.selected || options.labelsAlwaysVisible) {
+			  html = state.selected ? htmlTemplateSelect : htmlTemplateNormal;
+			  html = html.replace('$name', feature.properties.NAME);
+			  labelCanvas.drawLabel(html, shape, emptyStyle);
+			  
+			  if(state.selected)
+				alert("alarmSelected");
+			}
+		};
+		return eventPainter;
+	};
+	
+	function createUnitPainter(options) {
+		options = options || {};
+		var eventPainter = new FeaturePainter();
+		eventPainter.paintBody = function(geoCanvas, feature, shape, layer, map, state) {
+			shape.focusPoint.z = 20;
+			geoCanvas.drawIcon(shape.focusPoint, {
+			  width: 64 + "px",
+			  height: 64 + "px",
+			  url: state.selected?"themePic/unitSelect.png" : "themePic/unit.png"
+			  //anchorX : 0 + "px"
+			});
+		};
 
-	  var eventPainter = createEventPainter({labelsAlwaysVisible: true});
+		var emptyStyle = {
+			offset: 20,
+			pin: {
+			  endPosition: PinEndPosition.MIDDLE,
+			  width: 1
+			}
+		};
+		var htmlTemplateNormal = '<div style="background-color: rgb(40,40,40); padding: 5px; border-radius: 10px; color: rgb(238,233,233)">$name</div>';
+		var htmlTemplateSelect = '<div style="background-color: rgb(80,10,10); padding: 5px; border-radius: 10px; color: rgb(238,233,233)">$name</div>';
+		eventPainter.paintLabel = function(labelCanvas, feature, shape, layer, map, state) {
+			var html;
+			if (state.selected || options.labelsAlwaysVisible) {
+			  html = state.selected ? htmlTemplateSelect : htmlTemplateNormal;
+			  html = html.replace('$name', feature.properties.NAME);
+			  labelCanvas.drawLabel(html, shape, emptyStyle);
+			  
+			  if(state.selected){
+				alert("unitSelected");
+			  }
+			}
+		};
+		return eventPainter;
+	};
+	
+	function createIconFeatureLayer(layerName, layerStoreURL, layerPainter) {
+		/************************** Event Layer *****************************/
+		var layerStore = new UrlStore({
+			target: layerStoreURL
+		});
 
-	  var eventLayer = new FeatureLayer(eventModel, {
-		label: "Event",
-		layerType: LayerType.STATIC,
-		painter: eventPainter,
-		selectable: true
-	  });
-	  map.layerTree.addChild(eventLayer);
+		var layerModel = new FeatureModel(layerStore, {
+			reference: CRS84
+		});
+
+		var featureLayer = new FeatureLayer(layerModel, {
+			label: layerName,
+			layerType: LayerType.STATIC,
+			painter: layerPainter,
+			selectable: true
+			});
+		map.layerTree.addChild(featureLayer);
+		return featureLayer;
 	}
 
+	var eventFeatureLayer = createIconFeatureLayer("Event", "data/events.json", createEventPainter({labelsAlwaysVisible: true}));
+	var alarmFeatureLayer = createIconFeatureLayer("Event", "data/alarms.json", createAlarmPainter({labelsAlwaysVisible: true}));
+	var unitFeatureLayer = createIconFeatureLayer("Event", "data/units.json", createUnitPainter({labelsAlwaysVisible: true}));
+	//CustomLayerLoad End//
+
+	//tempCircleLayer Begin//
+	var tempCircleFeatureLayer;
+	function createTempCircle(centerPoint, Radius) {
+		var reference = CRS84;
+
+		var fillCirclePainter = new FeaturePainter();
+		fillCirclePainter.paintBody = function(geoCanvas, feature, shape, map, layer, state) {
+			geoCanvas.drawShape(shape, {
+			  stroke: {
+				color: "rgb(0,0,255)",
+				width: 5,
+				dash: [10, 5]
+			  },
+			  fill: {
+				color: ("rgba(43, 150, 33, 0.3)")
+			  },
+			zOrder: 0
+			});
+		};
+
+		tempCircleFeatureLayer = new FeatureLayer(new FeatureModel(new MemoryStore({
+		  data: [
+			new Feature(ShapeFactory.createCircleByCenterPoint(reference, centerPoint, Radius), {}, 1)
+		  ]
+		}), {
+		  reference: reference
+		}), {
+		  label: "Lines",
+		  painter: fillCirclePainter,
+		  selectable: true
+		});
+		
+		map.layerTree.addChild(tempCircleFeatureLayer);
+	}
+	
+	function removeTempCircle(){
+		if(tempCircleFeatureLayer != null)
+		map.layerTree.removeChild(tempCircleFeatureLayer);
+	}
+	//tempCircleLayer End//
+	
+	var count = 0;
+	var timer=window.setInterval(function(){
+		count++;
+		
+		if(unitFeatureLayer!=null)
+			map.layerTree.removeChild(unitFeatureLayer);
+	
+		
+		unitFeatureLayer = createIconFeatureLayer("tempRedlineCircle", "json/units" + count%7 + ".json", createUnitPainter({labelsAlwaysVisible: true}));
+		
+	},5000);
 
 	//Create model, layer, and add the result to the layer tree
 	var buildingsModel = createBuildingsModel();
 	var buildingsLayer = createBuildingsLayer(buildingsModel);
 	map.layerTree.addChild(buildingsLayer, "top");
-
-	createEventLayer();
 
 	//Fit on exact camera angle
 	map.mapNavigator.lookFrom(ShapeFactory.createPoint(ReferenceProvider.getReference("EPSG:4978"), [
